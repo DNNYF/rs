f2dd8c0d20fd9e0245e554f36959a80c2e0d6985
DENNY <albionkuonline600@gmail.com>
2024-09-10 14:03:05 +0700

    new branch

--- database/migrations/2024_08_20_090624_create_rawat_jalans_table.php
+++ database/migrations/2024_08_20_090624_create_rawat_jalans_table.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRawatJalanTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -25,4 +25,4 @@ class CreateRawatJalanTable extends Migration
     {
         Schema::dropIfExists('rawat_jalan');
     }
-}
+};
